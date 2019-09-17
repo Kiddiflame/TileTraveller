a2f75eaf5c30a1981bdd7f63d360470819998b83
unknown <kiddiflame@gmail.com>
2019-09-17 09:15:14 +0000

    Bjó til algorithma

---
+++ tile_traveller.py
@@ -0,0 +1,8 @@
+'''
+1. Búa til breytur til að geyma kortið
+2. Búa til pos breytu
+3. Búa til function sem segir manni hvar hann má fara og leyfir notenda að gera það
+4. Búa til if og else clause sem kemur í veg fyrir error-a
+5. Búa til while loopu-sem keyrir á meðan notandi er ekki búinn að vinna
+'''
+
